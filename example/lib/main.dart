535236b9ecb743428b8fe482a14f4be1a4bf0085
Iandi Santulus <iandisantulusn@gmail.com>
2021-12-26 20:12:22 +0700

    docs: add example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,62 +1,131 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
-
+import 'package:esc_pos_gen/esc_pos_gen.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image/image.dart' as img;
+
 import 'package:fluetooth/fluetooth.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      home: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  final Completer<CapabilityProfile> _profileCompleter =
+      Completer<CapabilityProfile>();
+
+  List<FluetoothDevice>? _devices;
+  FluetoothDevice? _connectedDevice;
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    CapabilityProfile.load().then(_profileCompleter.complete);
+    Fluetooth().getPairedDevices().then(
+      (List<FluetoothDevice> devices) {
+        setState(() => _devices = devices);
+      },
+    );
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await Fluetooth.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
+  Future<void> _print() async {
+    final CapabilityProfile profile = await _profileCompleter.future;
+    final Generator generator = Generator(PaperSize.mm58, profile);
+    final ByteData logoBytes = await rootBundle.load('assets/amd_logo.jpg');
+    final img.Image? decodedImg = await compute(
+      img.decodeJpg,
+      logoBytes.buffer.asUint8List(),
+    );
+    final img.Image resizedImg = img.copyResize(
+      decodedImg!,
+      width: 80,
+    );
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
+    final List<PosComponent> components = <PosComponent>[
+      PosImage(image: resizedImg),
+      const PosText.center('My Store'),
+      const PosSeparator(),
+      for (int i = 1; i <= 20; i++)
+        PosRow.leftRightText(
+          leftText: 'Product $i',
+          rightText: 'Rp. $i',
+        ),
+      const PosSeparator(),
+      PosBarcode.code128(
+        '{A12345'.split(''),
+      ),
+      const PosSeparator(),
+      const PosFeed(1),
+      const PosCut(),
+    ];
 
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+    final Paper paper = Paper(
+      generator: generator,
+      components: components,
+    );
+
+    Fluetooth().sendBytes(paper.bytes);
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          ElevatedButton(
+            onPressed: _connectedDevice != null ? _print : null,
+            child: const Text('Print'),
+          ),
+        ],
       ),
+      body: _devices == null
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemBuilder: (_, int index) {
+                final FluetoothDevice currentDevice = _devices![index];
+                return ListTile(
+                  title: Text(currentDevice.name),
+                  subtitle: Text(currentDevice.address),
+                  trailing: ElevatedButton(
+                    onPressed: _connectedDevice?.address ==
+                            currentDevice.address
+                        ? () {
+                            Fluetooth().disconnect();
+                            setState(() => _connectedDevice = null);
+                          }
+                        : _connectedDevice == null
+                            ? () {
+                                Fluetooth().connect(currentDevice.address).then(
+                                  (FluetoothDevice device) {
+                                    setState(() {
+                                      _connectedDevice = device;
+                                    });
+                                  },
+                                );
+                              }
+                            : null,
+                    child: Text(
+                      _connectedDevice?.address == currentDevice.address
+                          ? 'Disconnect'
+                          : 'Connect',
+                    ),
+                  ),
+                );
+              },
+              itemCount: _devices!.length,
+            ),
     );
   }
 }
